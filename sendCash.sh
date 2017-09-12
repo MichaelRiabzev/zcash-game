ae5531e40d1043adcd347271cb7241a66537cb42
Michael Riabzev <RiabzevMichael@gmail.com>
2017-09-12 12:40:25 +0300

    sending amount 777

--- sendCash.sh
+++ sendCash.sh
@@ -1,3 +1,3 @@
-zcash-cli z_sendmany `grep ^t addresses.txt` "[{\"address\":\"`grep ^z addresses.txt | sort -R | head -n 1`\",\"amount\":0.0001}]" > opid.txt
+zcash-cli z_sendmany `grep ^t addresses.txt` "[{\"address\":\"`grep ^z addresses.txt | sort -R | head -n 1`\",\"amount\":0.00777}]" > opid.txt
 sleep 70
 zcash-cli z_getoperationstatus "[\"`cat opid.txt`\"]" | grep txid | cut -d'"' -f4
